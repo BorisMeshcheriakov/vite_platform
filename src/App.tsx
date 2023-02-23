e5b97232aecd3330172371e320383e7d4e39014d
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-23 17:39:25 +0300

    feat: mui and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { Add } from '@mui/icons-material';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</div>
+		<Grid container spacing={2}>
+			<Grid item xs={1}>
+				<Button variant="outlined">Test</Button>
+			</Grid>
+			<Grid item xs={1}>
+				<IconButton>
+					<Add />
+				</IconButton>
+			</Grid>
+			<Grid item xs={1}></Grid>
+			<Grid item xs={1}></Grid>
+		</Grid>
 	);
 }
 
