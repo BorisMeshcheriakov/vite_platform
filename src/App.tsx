0811c31eb85a9baff002ad021bf00adee16ceb8f
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-24 17:49:41 +0300

    fix: Mui removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { Add } from '@mui/icons-material';
+import { Button } from './components/Button';
+
+import st from './App.module.scss';
 
 function App() {
 	return (
-		<Grid container spacing={2}>
-			<Grid item xs={1}>
-				<Button variant="outlined">Test</Button>
-			</Grid>
-			<Grid item xs={1}>
-				<IconButton>
-					<Add />
-				</IconButton>
-			</Grid>
-			<Grid item xs={1}></Grid>
-			<Grid item xs={1}></Grid>
-		</Grid>
+		<div className={st.app}>
+			<section className={st.row}>
+				<h2>Buttons</h2>
+				<div className={st.container}>
+					<Button>test</Button>
+				</div>
+			</section>
+		</div>
 	);
 }
 
