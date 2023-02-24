08fe679a8773d1f5beb95b7b0a40d8cba446c1b2
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-24 17:50:02 +0300

    feat: Sass, default styles

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children?: React.ReactNode;
+}
+
+export const Button: React.FC<Props> = ({ children, ...props }) => {
+	return <button {...props}>{children}</button>;
+};
