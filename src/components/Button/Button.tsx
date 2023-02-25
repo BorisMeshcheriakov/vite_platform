81b40a229faa6c637b107fa88d13ce57a257a66a
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-25 15:33:17 +0300

    feat: Common button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import st from './Button.module.scss';
+
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	children?: React.ReactNode;
 }
 
 export const Button: React.FC<Props> = ({ children, ...props }) => {
-	return <button {...props}>{children}</button>;
+	return (
+		<button className={st.btn} {...props}>
+			{children}
+		</button>
+	);
 };
